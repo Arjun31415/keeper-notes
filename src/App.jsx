772fca88236f1347c882d39f2dab0ea50b869edf
Arjun31415 <arjunp0710@gmail.com>
2021-10-13 18:18:34 +0530

    Finish Part 2 of the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "./notes";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((x) => {
+        return <Note key={x.key} title={x.title} content={x.content} />;
+      })}
       <Footer />
     </div>
   );
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Note() {
+export default function Note(prop) {
   return (
     <div className="note">
-      <h1>title</h1>
-      <p>content</p>
+      <h1>{prop.title}</h1>
+
+      <p> {prop.content} </p>
     </div>
   );
 }
